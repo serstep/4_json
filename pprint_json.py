979989ecde73201c216fcd897d0ea34d91a7c82e
root <root@localhost.localdomain>
2016-12-18 13:33:26 +0200

    Написан скрипт для форматирования json файлов

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,28 @@
-import json
+import json, sys
 
 
 def load_data(filepath):
-    pass
+    try:
+        data = ""
+        with open(filepath) as file:
+            data = json.load(file)
+        return data
+    except:
+        return None
 
 
 def pretty_print_json(data):
-    pass
-
+    return json.dumps(data, default="Ошибка обработка данных", indent=4)
+    
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        print("Укажите файл с данными в качестве параметра")
+        exit()
+
+    data = load_data(sys.argv[1])
+    if data is None:
+        print("Невозможно оработать файл")
+        exit()
+
+    print(pretty_print_json(data))
